c1e2c65b2795f448847697ee67302365b150ea7b
AlexStorm <info@alexstorm.ru>
2022-12-05 08:23:51 +0530

    pathes fixes

--- src/lib/fui-grid.module.ts
+++ src/lib/fui-grid.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { SInfinityTableComponent } from "./sinfinity-table/sinfinity-table.component";
-import { InfiniteScrollModule } from "ngx-infinite-scroll";
+
 import { FuiIconsModule } from "@solenopsys/uimatrix-icons";
 import { FormsModule } from "@angular/forms";
 import { UtilsModule } from "@solenopsys/uimatrix-utils";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
 import { AngularResizeEventModule } from "angular-resize-event";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-  declarations: [SInfinityTableComponent],
   imports: [CommonModule, InfiniteScrollModule, FuiIconsModule, FormsModule, UtilsModule, AngularResizeEventModule],
+
+  declarations: [SInfinityTableComponent],
   exports: [
     SInfinityTableComponent
   ]
