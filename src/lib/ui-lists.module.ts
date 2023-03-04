795d8c95183e24d78934ea6b3d28c3a0c2c1fd94
AlexStorm <info@alexstorm.ru>
2023-03-04 17:28:49 +0530

    build fixes

--- src/lib/ui-lists.module.ts
+++ src/lib/ui-lists.module.ts
@@ -1,25 +1,33 @@
-import { NgModule } from "@angular/core";
-import { InfinityTableComponent } from "./infinity-table/infinity-table.component";
+import {NgModule} from "@angular/core";
+import {InfinityTableComponent} from "./infinity-table/infinity-table.component";
 
-import { UIIconsModule } from "@solenopsys/ui-icons";
-import { FormsModule } from "@angular/forms";
+import {UIIconsModule} from "@solenopsys/ui-icons";
+import {FormsModule} from "@angular/forms";
 import {DeclaredService, UtilsModule} from "@solenopsys/ui-utils";
-import { InfiniteScrollModule } from "ngx-infinite-scroll";
-import { AngularResizeEventModule } from "angular-resize-event";
-import { CommonModule } from "@angular/common";
+import {InfiniteScrollModule} from "ngx-infinite-scroll";
+import {AngularResizeEventModule} from "angular-resize-event";
+import {CommonModule} from "@angular/common";
+import {UIControlsModule} from "@solenopsys/ui-controls";
 
 const components = [InfinityTableComponent];
 
 @NgModule({
-  imports: [CommonModule, InfiniteScrollModule, UIIconsModule, FormsModule, UtilsModule, AngularResizeEventModule],
+    imports: [
+        CommonModule,
+        InfiniteScrollModule,
+        UIIconsModule,
+        FormsModule,
+        UtilsModule,
+        AngularResizeEventModule,
+        UIControlsModule],
 
-  declarations: components,
-  exports: [
-    InfinityTableComponent
-  ]
+    declarations: components,
+    exports: [
+        InfinityTableComponent
+    ]
 })
 export class UIListsModule {
-  constructor(private ds: DeclaredService) {
-    ds.addComps("@solenopsys/ui-lists", components)
-  }
+    constructor(private ds: DeclaredService) {
+        ds.addComps("@solenopsys/ui-lists", components)
+    }
 }
